26d3c1700341b3605b4fd80c4a8b42dfec5b4856
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2012-04-26 22:59:10 -0400

    * Fully running example of using ajaxInvoke, one button works, the other does not.

---
+++ README.md
@@ -0,0 +1,14 @@
+sbt is included so run:
+
+
+    ./sbt.sh
+    >container:start
+
+then go to
+
+    http://127.0.0.1:8080
+
+And you will see two buttons, one does a full POST, so it produces a page reload, the other button does an ajax submit, so the whole page
+is not reloaded.
+
+
---
+++ src/main/scala/com/fmpwizard/ajaxInvoke/snippet/ajaxInvoke.scala
@@ -0,0 +1,15 @@
+package com.fmpwizard.ajaxInvoke.snippet
+
+import net.liftweb.http.{S, SHtml}
+import net.liftweb.http.js.JsCmds
+import net.liftweb.util.Helpers._
+class ajaxInvoke{
+  def render = {
+    "#button [onclick]" #> SHtml.ajaxInvoke{ () =>
+      JsCmds.Alert("Got it!")
+    }._2.cmd &
+    "#button2 [onclick]" #> (SHtml.ajaxInvoke{ () =>
+      JsCmds.Alert("Got it!")
+    }._2.cmd.toJsCmd + "return false;")
+  }
+}
